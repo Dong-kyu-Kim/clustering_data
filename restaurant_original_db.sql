50b763061a0b3f6d8858f30561927965bfc93f2e
Dongkyu_Kim <154038146+Dong-kyu-Kim@users.noreply.github.com>
2024-12-02 19:13:35 -0700

    code with results

--- restaurant_original_db.sql
+++ restaurant_original_db.sql
@@ -1,21 +1,21 @@
 CREATE TABLE restaurant_data (
-    Name VARCHAR(30),
-    Location VARCHAR(30),
-    Cuisine VARCHAR(30),
-    Rating FLOAT,
-    Seating_Capacity INT,
-    Average_Meal_Price FLOAT,
-    Marketing_Budget FLOAT,
-    Social_Media_Followers INT,
-    Chef_Experience_Years INT,
-    Number_of_Reviews INT,
-    Avg_Review_Length FLOAT,
-    Ambience_Score FLOAT,
-    Service_Quality_Score FLOAT,
-    Parking_Availability VARCHAR(15),
-    Weekend_Reservations INT,
-    Weekday_Reservations INT,
-    Revenue FLOAT
+    "Name" VARCHAR(30),
+    "Location" VARCHAR(30),
+    "Cuisine" VARCHAR(30),
+    "Rating" FLOAT,
+    "Seating Capacity" INT,
+    "Average Meal Price" FLOAT,
+    "Marketing Budget" FLOAT,
+    "Social Media Followers" INT,
+    "Chef Experience Years" INT,
+    "Number of Reviews" INT,
+    "Avg Review Length" FLOAT,
+    "Ambience Score" FLOAT,
+    "Service Quality Score" FLOAT,
+    "Parking Availability" VARCHAR(15),
+    "Weekend Reservations" INT,
+    "Weekday Reservations" INT,
+    "Revenue" FLOAT
 );
 
 SELECt * FROM restaurant_data;
